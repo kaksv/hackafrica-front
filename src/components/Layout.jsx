91ab1ddc7b0f033dc1be7f732929c5115c6f0af0
kaksv <kakoozavian@gmail.com>
2025-03-30 13:32:00 +0300

    loadings

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import MainView from "./MainView"
 
 const Layout = ({ children, onSearch }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen min-w-screen bg-gray-50">
       <Navbar onSearch={onSearch} />
       <div className="flex flex-1">
         <Sidebar />
