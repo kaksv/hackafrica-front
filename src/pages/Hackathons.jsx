1e03feca9b18c8ab64526add48b500c851b1615f
kaksv <kakoozavian@gmail.com>
2025-04-27 11:52:08 +0300

    design

--- src/pages/Hackathons.jsx
+++ src/pages/Hackathons.jsx
@@ -229,10 +229,10 @@ const Hackathons = () => {
                   <button
                     onClick={() => handleParticipate(hackathon._id)}
                     disabled={participationStatus[hackathon._id]}
-                    className={`px-4 py-2 rounded-lg transition-colors border ${
+                    className={`px-4 py-2 rounded-lg transition-colors border cursor-pointer ${
                       participationStatus[hackathon._id]
                         ? "bg-gray-300 text-green-800 border border-green-300 cursor-not-allowed"
-                        : "bg-blue-600 hover:bg-blue-700 text-black border-blue-600 hover:border-blue-700"
+                        : "bg-blue-600 hover:bg-blue-700 text-white border-blue-600 hover:border-blue-700"
                     }`}
                   >
                     {participationStatus[hackathon._id] ? (
