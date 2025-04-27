64d43eab69dfcaf26798503987bd827212041ff4
kaksv <kakoozavian@gmail.com>
2025-04-27 12:10:30 +0300

    design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -184,7 +184,7 @@ const Navbar = ({ onSearch }) => {
               </Link>
               <Link
                 to="/register"
-                className="w-full inline-flex justify-center items-center px-4 py-2 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="w-full inline-flex justify-center items-center px-4 py-2 border border-transparent text-md font-bold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Register
               </Link>
